80c277919746b50426fdcf88f426c06d38b3a6fb
gruvw <gruvw.dev@gmail.com>
2022-03-06 12:47:09 +0100

    Example test from PDF

--- test/ch/epfl/javelo/data/GraphEdgesTest.java
+++ test/ch/epfl/javelo/data/GraphEdgesTest.java
@@ -3,77 +3,101 @@ package ch.epfl.javelo.data;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
+import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class GraphEdgesTest {
+class GraphEdgesTest {
 
     private static final double DELTA = 1e-10;
-    ByteBuffer edgesBuffer;
-    IntBuffer profileIds;
-    ShortBuffer elevations;
-    GraphEdges edges;
 
-    @BeforeEach
-    public void init() {
-        edgesBuffer = ByteBuffer.allocate(10);
-        edgesBuffer.putInt(0, -12);
-        edgesBuffer.putShort(4, (short) 0x10_b);
-        edgesBuffer.putShort(6, (short) 0x10_0);
+    private static GraphEdges edges;
+
+    @BeforeAll
+    static void initGlobalVars() {
+        ByteBuffer edgesBuffer = ByteBuffer.allocate(10);
+        edgesBuffer.putInt(0, ~12);
+        edgesBuffer.putShort(4, (short) 0x10B);
+        edgesBuffer.putShort(6, (short) 0x100);
         edgesBuffer.putShort(8, (short) 2022);
+        IntBuffer profileIds = IntBuffer.wrap(new int[] {(3 << 30) | 1});
+        ShortBuffer elevations = ShortBuffer.wrap(
+                new short[] {(short) 0, (short) 0x180C, (short) 0xFEFF, (short) 0xFFFE,
+                             (short) 0xF000});
 
-        profileIds = IntBuffer.wrap(new int[] {(3 << 30) | 1});
-
-        elevations = ShortBuffer.wrap(new short[] {(short) 0, (short) 0x180C, (short) 0xFEFF,
-                                                   (short) 0xFFFE, (short) 0xF000});
-
-        edges = new GraphEdges(edgesBuffer, profileIds, elevations);
+        edges = new GraphEdges(edgesBuffer.asReadOnlyBuffer(), profileIds.asReadOnlyBuffer(),
+                elevations.asReadOnlyBuffer());
     }
 
-    @Test
-    public void isInvertedTest() {
-        assertTrue(edges.isInverted(0));
+    static Stream<Arguments> isInvertedTest() {
+        return Stream.of(Arguments.of(edges, 0, true));
     }
 
     @ParameterizedTest
-    @CsvSource({"0,12"})
-    public void targetNodeIdTest(int input, int expected) {
+    @MethodSource
+    void isInvertedTest(GraphEdges edges, int edgeId, boolean expected) {
+        assertEquals(expected, edges.isInverted(edgeId));
+    }
+
+    static Stream<Arguments> targetNodeIdTest() {
+        return Stream.of(Arguments.of(edges, 0, 12));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void targetNodeIdTest(GraphEdges edges, int edgeId, int expected) {
         int actual = edges.targetNodeId(0);
         assertEquals(expected, actual);
     }
 
-    @ParameterizedTest
-    @CsvSource({"0,16.6875"})
-    public void lengthTest(int input, double expected) {
-        double actual = edges.length(0);
-        assertEquals(expected, actual, DELTA);
+    static Stream<Arguments> lengthTest() {
+        return Stream.of(Arguments.of(edges, 0, 16.6875));
     }
 
     @ParameterizedTest
-    @CsvSource({"0,16.0"})
-    public void elevationGainTest(int input, double expected) {
-        double actual = edges.elevationGain(0);
+    @MethodSource
+    void lengthTest(GraphEdges edges, int edgeId, double expected) {
+        double actual = edges.length(edgeId);
         assertEquals(expected, actual, DELTA);
     }
 
+    static Stream<Arguments> elevationGainTest() {
+        return Stream.of(Arguments.of(edges, 0, 16));
+    }
+
     @ParameterizedTest
-    @CsvSource({"0,2022"})
-    public void attributesIndexTest(int input, int expected) {
-        int actual = edges.attributesIndex(0);
+    @MethodSource
+    void elevationGainTest(GraphEdges edges, int edgeId, double expected) {
+        double actual = edges.elevationGain(edgeId);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    static Stream<Arguments> attributesIndexTest() {
+        return Stream.of(Arguments.of(edges, 0, 2022));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void attributesIndexTest(GraphEdges edges, int edgeId, int expected) {
+        int actual = edges.attributesIndex(edgeId);
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void profileSamplesTest() {
-        init();
-        float[] expectedSamples = new float[] {384.0625f, 384.125f, 384.25f, 384.3125f, 384.375f,
-                                               384.4375f, 384.5f, 384.5625f, 384.6875f, 384.75f};
-        assertArrayEquals(expectedSamples, edges.profileSamples(0));
+    static Stream<Arguments> profileSamplesTest() {
+        return Stream.of(Arguments.of(edges, 0,
+                new float[] {384.0625F, 384.125F, 384.25F, 384.3125F, 384.375F, 384.4375F, 384.5F,
+                             384.5625F, 384.6875F, 384.75F}));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void profileSamplesTest(GraphEdges edges, int edgeId, float[] expected) {
+        float[] actual = edges.profileSamples(edgeId);
+        assertArrayEquals(expected, actual);
     }
 
 }
--- test/ch/epfl/javelo/data/GraphNodesTest.java
+++ test/ch/epfl/javelo/data/GraphNodesTest.java
@@ -3,56 +3,75 @@ package ch.epfl.javelo.data;
 import java.nio.IntBuffer;
 import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class GraphNodesTest {
+class GraphNodesTest {
 
-    private static final double DELTA = 1e-6;
+    private static final double DELTA = 1e-11;
 
-    private static Stream<Arguments> countTest() {
+    private static GraphNodes nodes;
+
+    @BeforeAll
+    static void initGlobalVars() {
         IntBuffer b = IntBuffer.wrap(new int[] {2_600_000 << 4, 1_200_000 << 4, 0x2_000_1234});
-        return Stream.of(Arguments.of(new GraphNodes(b), 1));
+        nodes = new GraphNodes(b.asReadOnlyBuffer());
+    }
+
+    static Stream<Arguments> countTest() {
+        return Stream.of(Arguments.of(nodes, 1));
     }
 
     @ParameterizedTest
     @MethodSource
-    public void countTest(GraphNodes gn, int expected) {
-        assertEquals(expected, gn.count());
+    void countTest(GraphNodes nodes, int expected) {
+        assertEquals(expected, nodes.count());
     }
 
-    private static Stream<Arguments> nodeETest() {
-        IntBuffer b = IntBuffer.wrap(new int[] {2_600_000 << 4, 1_200_000 << 4, 0x2_000_1234});
-        return Stream.of(Arguments.of(new GraphNodes(b), 0, 2_600_000));
+    static Stream<Arguments> nodeETest() {
+        return Stream.of(Arguments.of(nodes, 0, 2_600_000));
     }
 
     @ParameterizedTest
     @MethodSource
-    public void nodeETest(GraphNodes gn, int input, int expected) {
-        double actual = gn.nodeE(input);
+    void nodeETest(GraphNodes nodes, int nodeId, int expected) {
+        double actual = nodes.nodeE(nodeId);
         assertEquals(expected, actual, DELTA);
     }
 
-    // @ParameterizedTest
-    // @MethodSource("provideBuffersForNodeN")
-    // public void nodeNTest(int input, int expected) {
-    // double actual = ns1.nodeN(input);
-    // assertEquals(expected, actual, DELTA);
-    // }
+    static Stream<Arguments> nodeNTest() {
+        return Stream.of(Arguments.of(nodes, 0, 1_200_000));
+    }
 
-    // @ParameterizedTest
-    // @CsvSource({"0,2"})
-    // public void outDegreeTest(int input, int expected) {
-    // double actual = ns1.outDegree(0);
-    // assertEquals(expected, actual, DELTA);
-    // }
+    @ParameterizedTest
+    @MethodSource
+    void nodeNTest(GraphNodes nodes, int nodeId, int expected) {
+        double actual = nodes.nodeN(nodeId);
+        assertEquals(expected, actual, DELTA);
+    }
 
-    // @ParameterizedTest
-    // @CsvSource({"0,0,0x1234", "0,1,0x1235"})
-    // public void edgeIdTest(int input1, int input2, int expected) {
-    // double actual = ns1.edgeId(input1, input2);
-    // assertEquals(expected, actual, DELTA);
-    // }
+    static Stream<Arguments> outDegreeTest() {
+        return Stream.of(Arguments.of(nodes, 0, 2));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void outDegreeTest(GraphNodes nodes, int nodeId, int expected) {
+        int actual = nodes.outDegree(nodeId);
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> edgeIdTest() {
+        return Stream.of(Arguments.of(nodes, 0, 0, 0x1234), Arguments.of(nodes, 0, 1, 0x1235));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void edgeIdTest(GraphNodes nodes, int nodeId, int edgeIndex, int expected) {
+        double actual = nodes.edgeId(nodeId, edgeIndex);
+        assertEquals(expected, actual, DELTA);
+    }
 
 }
