9f361ddc8a4b5ed35384596583d01a33854cfc54
Florian Kolly <florian.kolly@epfl.ch>
2022-03-08 09:17:12 +0100

    GraphSectors testing
    Co-authored-by: Lucas Jung <gruvw.dev@gmail.com>

--- test/ch/epfl/javelo/data/GraphSectorsTest.java
+++ test/ch/epfl/javelo/data/GraphSectorsTest.java
@@ -15,25 +15,37 @@ class GraphSectorsTest {
 
     @BeforeAll
     static void initGlobalVars() {
-        ByteBuffer b = ByteBuffer.allocate(12);
-        b.putInt(0, 1); // first node id of the sector 0
-        b.putShort(4, (short) 2); // number of nodes in the sector 0
-        b.putInt(6, 5); // first node id of the sector 1
-        b.putShort(10, (short) 3); // number of nodes in the sector 1
+        ByteBuffer b = ByteBuffer.allocate((Integer.SIZE + Short.SIZE) * 128 * 128);
+        for (int i = 0; i < 128 * 128; i++) {
+            b.putInt(i * (Integer.BYTES + Short.BYTES), i);
+            b.putShort(4 + i * (Integer.BYTES + Short.BYTES), (short) 1);
+        }
         gs = new GraphSectors(b.asReadOnlyBuffer());
     }
 
     @Test
     void sectorsInAreaTest() {
         PointCh bottomLeft = new PointCh(SwissBounds.MIN_E, SwissBounds.MIN_N);
-        GraphSectors.Sector s0 = new GraphSectors.Sector(1, 3);
-        GraphSectors.Sector s1 = new GraphSectors.Sector(3, 6);
+        PointCh topRight = new PointCh(SwissBounds.MAX_E, SwissBounds.MAX_N);
+        PointCh middle = new PointCh((SwissBounds.MAX_E + SwissBounds.MIN_E) / 2. -2, (SwissBounds.MAX_N + SwissBounds.MIN_N) / 2. - 2);
+        GraphSectors.Sector s0 = new GraphSectors.Sector(0, 1);
+        GraphSectors.Sector s1 = new GraphSectors.Sector(1, 2);
+        GraphSectors.Sector s8192 = new GraphSectors.Sector(8192,8193);
+        GraphSectors.Sector s128 = new GraphSectors.Sector(127, 128);
+        GraphSectors.Sector sLast = new GraphSectors.Sector(128 * 128 - 1, 128 * 128);
+        // Bottom left
         List<GraphSectors.Sector> expected = new ArrayList<>();
         expected.add(s0);
-        List<GraphSectors.Sector> actual = gs.sectorsInArea(bottomLeft, 1);
-        assertEquals(expected, actual);
-        actual = gs.sectorsInArea(bottomLeft, SwissBounds.WIDTH);
-        expected.add(s1);
+        List<GraphSectors.Sector> actual = gs.sectorsInArea(bottomLeft, 0);
+        // Top right
+        // expected = new ArrayList<>();
+        // expected.add(sLast);
+        // actual = gs.sectorsInArea(topRight, 0);
+        // assertEquals(expected, actual);
+        // Middle
+        expected = new ArrayList<>();
+        expected.add(s8192);
+        actual = gs.sectorsInArea(middle, 1);
         assertEquals(expected, actual);
     }
 
