bc41e2cc50b741860b8faac2bb9bfb8a20372b4a
gruvw <gruvw.dev@gmail.com>
2022-03-26 21:23:11 +0100

    Don't create Edge instances @593

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -8,6 +8,7 @@ import java.util.PriorityQueue;
 import ch.epfl.javelo.Preconditions;
 import ch.epfl.javelo.data.Graph;
 import ch.epfl.javelo.data.GraphNodes;
+import ch.epfl.javelo.projection.PointCh;
 
 /**
  * Represents a route planning.
@@ -61,39 +62,47 @@ public final class RouteComputer {
 
         }
 
+        // FIXME: allowed to create a Tuple and use it in the hashMap ? nodeId -> (
+        // fromNodeId, edgeId)
+        record FromNode(int fromNodeId, int edgeId) {
+        }
+
         Preconditions.checkArgument(startNodeId != endNodeId);
         PriorityQueue<WeightedNode> toVisit = new PriorityQueue<WeightedNode>();
         // FIXME: I don't init distances to inf and previous to 0 so that I don't loop here
-        // FIXME: allowed to create a Tuple and use it in the hashMap ? nodeId -> (distance, Edge)
         HashMap<Integer, Float> visited = new HashMap<Integer, Float>(); // nodeId -> distance
-        HashMap<Integer, Edge> previous = new HashMap<Integer, Edge>(); // toNodeId -> Edge
+        HashMap<Integer, FromNode> previous = new HashMap<Integer, FromNode>(); // toNodeId ->
+                                                                                // FromNode
         toVisit.add(new WeightedNode(startNodeId, 0));
+        // PointCh endPoint = graph.nodePoint(endNodeId);
         int i = 0;
         while (!toVisit.isEmpty()) {
             i++;
             WeightedNode current = toVisit.poll();
             if (current.nodeId == endNodeId) {
                 List<Edge> edges = new ArrayList<Edge>();
-                int nodeId = current.nodeId;
+                int toNodeId = endNodeId;
                 do {
-                    Edge edge = previous.get(nodeId);
+                    FromNode fromNode = previous.get(toNodeId);
+                    Edge edge = Edge.of(graph, fromNode.edgeId, fromNode.fromNodeId, toNodeId);
                     edges.add(edge);
-                    nodeId = edge.fromNodeId();
-                } while (nodeId != startNodeId);
+                    toNodeId = fromNode.fromNodeId;
+                } while (toNodeId != startNodeId);
                 System.out.println(i);
                 return new SingleRoute(edges);
             }
             int outDegree = graph.nodeOutDegree(current.nodeId);
+            PointCh fromPoint = graph.nodePoint(current.nodeId);
             for (int edgeIndex = 0; edgeIndex < outDegree; edgeIndex++) {
                 int edgeId = graph.nodeOutEdgeId(current.nodeId, edgeIndex);
                 int toNodeId = graph.edgeTargetNodeId(edgeId);
-                Edge edge = Edge.of(graph, edgeId, current.nodeId, toNodeId);
+                PointCh toPoint = graph.nodePoint(toNodeId);
                 double cost = costFunction.costFactor(current.nodeId, edgeId);
                 // FIXME: why cast ? why weightedNode distance is not double ?
-                float distance = current.distance + (float) (cost * edge.length());
+                float distance = current.distance + (float) (cost * toPoint.distanceTo(fromPoint));
                 if (!visited.containsKey(toNodeId) || distance < visited.get(toNodeId)) {
                     visited.put(toNodeId, distance);
-                    previous.put(toNodeId, edge);
+                    previous.put(toNodeId, new FromNode(current.nodeId, edgeId));
                     toVisit.add(new WeightedNode(toNodeId, distance));
                 }
             }
