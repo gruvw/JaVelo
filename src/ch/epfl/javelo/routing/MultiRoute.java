5d4de7e1429f4e0b9196fad1a22f5de5fcbe8dc5
Florian Kolly <florian.kolly@epfl.ch>
2022-03-30 17:06:36 +0200

    Cleaner MultiRoute + fixme

--- src/ch/epfl/javelo/routing/MultiRoute.java
+++ src/ch/epfl/javelo/routing/MultiRoute.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 import ch.epfl.javelo.Math2;
 import ch.epfl.javelo.Preconditions;
 import ch.epfl.javelo.projection.PointCh;
@@ -35,13 +34,15 @@ public final class MultiRoute implements Route {
         Preconditions.checkArgument(!segments.isEmpty());
         this.segments = List.copyOf(segments);
         List<Edge> edges = new ArrayList<>();
+        // FIXME: Check if it is a good way to do it
         Set<PointCh> points = new LinkedHashSet<>();
         runningLengths = new double[this.segments.size() + 1];
         runningLengths[0] = 0;
         for (int i = 0; i < segments.size(); i++) {
-            edges.addAll(this.segments.get(i).edges());
-            points.addAll(this.segments.get(i).points());
-            runningLengths[i + 1] = runningLengths[i] + this.segments.get(i).length();
+            Route segment = this.segments.get(i);
+            edges.addAll(segment.edges());
+            points.addAll(segment.points());
+            runningLengths[i + 1] = runningLengths[i] + segment.length();
         }
         this.edges = List.copyOf(edges);
         this.points = List.copyOf(new ArrayList<>(points));
